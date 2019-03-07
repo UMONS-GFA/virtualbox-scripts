0b51d8476cf911fdfcadff926ad6bbe28dfb181f
bastinc <christophe.bastin@umons.ac.be>
2019-03-07 09:57:02 +0100

    add retext and pandoc

--- upgrade_ubuntu.sh
+++ upgrade_ubuntu.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Updating Ubuntu system…"
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+apt-get update
+apt-get -y upgrade
+apt-get -y dist-upgrade
 
 echo "Install requirements for python "
-sudo apt-get -y install python3-pip python3-dev
+apt-get -y install python3-pip python3-dev
 
 echo "Installing GDAL"
-sudo apt-get -y install gdal-bin libgdal-dev
+apt-get -y install gdal-bin libgdal-dev
 
 echo "Installing QGIS"
-sudo apt-get -y install qgis
+apt-get -y install qgis
 
 echo "Installing sqlite…"
-sudo apt-get -y install sqlite3
+apt-get -y install sqlite3
+
+echo "Installing markdown tools…"
+apt-get -y install retext pandoc
